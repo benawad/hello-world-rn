6f4059fa274e44467ef69bde4197d6dd341d71d4
ben awad <benawad97@gmail.com>
2017-04-12 14:26:56 -0500

    basic setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {
+  Text
+} from 'react-native';
+
+export default () => {
+  console.log('HELLO I AM LOGGING');
+  return (
+    <Text>Hello World!</Text>
+  );
+};
